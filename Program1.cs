1acdd8447ec54cbf4e5b759e90233c5c67d7a6ec
phhkhuong18i1 <48077379+phhkhuong18i1@users.noreply.github.com>
2019-02-28 11:47:14 +0700

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace bai1
+{
+    class Program
+    {
+        public  void Main (string[] args)
+        {
+            tinhtong s = new tinhtong();
+            s.nhap();
+            s.tinhTong1();
+            s.tinhTong2();
+            s.tinhTong3();
+            s.tongUoc();
+            s.inTong();
+            Console.ReadLine();
+
+        }
+    }
+
+}
---
+++ tinhtong.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    class tinhtong
+    {
+        int n;
+        public void nhap()
+        {
+            Console.WriteLine("Nhap n: ");
+            this.n = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public float tinhTong1()
+        {
+            float S1 = 0;
+            float S2 = 0;
+
+            for (int i = 1; i <= this.n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    S1 = S1 + -(i);
+                }
+
+                if (i % 2 != 0)
+                {
+                    S2 = S2 + i;
+
+                }
+            }
+            return S1 + S2;
+        }
+
+
+
+
+
+        public float tinhTong2()
+        {
+            float S3 = 0;
+            for (int i = 1; i <= this.n; i++)
+                S3 = S3 + 1 / (i);
+            return S3;
+        }
+
+        public float tinhTong3()
+        {
+            float S4 = 0;
+            for (int i = 0; i <= this.n; i++)
+                S4 = S4 + (this.n - i);
+            return S4;
+        }
+
+        public float tongUoc()
+        {
+            float S5 = 0;
+            for (float i = 1; i < this.n; i++)
+            {
+                if (this.n % i == 0)
+                {
+                    S5 = S5 + i;
+                }
+
+            }
+            return S5;
+        }
+        public void inTong()
+        {
+            Console.WriteLine("Tong  1 la:{0}", this.tinhTong1());
+
+            Console.WriteLine("Tong 2 la:{0}", this.tinhTong2());
+
+            Console.WriteLine("Tong 3 la:{0}", this.tinhTong3());
+
+            Console.WriteLine("Tong Uoc la:{0}", this.tongUoc());
+
+        }
+    }
+}
+    
+
