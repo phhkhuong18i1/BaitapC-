725c2373c3cc2c694e1540e38dff7c0dbb5fed50
phhkhuong18i1 <48077379+phhkhuong18i1@users.noreply.github.com>
2019-04-04 20:19:29 +0700

    Add files via upload
    nop bai ngay 4/4

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace khuongxu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtGT_Click(object sender, RoutedEventArgs e)
+        {
+            int n = Int32.Parse(txtSoN.Text);
+            int gt = 1;
+            for(int i=2;i<=n;i++)
+            {
+                gt = gt * i;
+            }
+            txtKq.Text = gt.ToString();
+        }
+
+        private void Btcb2_Click(object sender, RoutedEventArgs e)
+        {
+            int n = Int32.Parse(txtSoN.Text);
+            double s = 0;
+            for(int i=1;i<=n;i++)
+            {
+                s = s + Math.Sqrt(i);
+            }
+            txtKq.Text = s.ToString();
+        }
+
+        private void Bttm_Click(object sender, RoutedEventArgs e)
+        {
+            int x = Int32.Parse(txtSox.Text);
+            int n = Int32.Parse(txtSoN.Text);
+            double s = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                s = s + Math.Pow(x,i);
+            }
+            txtKq.Text = s.ToString();
+        }
+    }
+}
---
+++ MainWindow.xaml1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mang
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtGTNN_Click(object sender, RoutedEventArgs e)
+        {
+            string n =txtmang.Text;
+            
+            char[] c = n.ToCharArray();
+            int[] ch = new int[c.Length];
+            for (int i = 0; i < ch.Length; i++)
+            {
+                ch[i] = (int)c[i] - '0';
+            }
+            int min = ch[0];
+            for(int i=0;i<c.Length;i++)
+            {
+                if(min>ch[i])
+                {
+                    min = ch[i];
+                }
+            }
+            txtGTNN.Text = min.ToString();
+
+        }
+
+        private void BtGTLN_Click(object sender, RoutedEventArgs e)
+        {
+            string n = txtmang.Text;
+            char[] c = n.ToCharArray();
+            int[] ch = new int[c.Length];
+            for (int i = 0; i < ch.Length; i++)
+            {
+                ch[i] = (int)c[i] - '0';
+            }
+            int max = ch[0];
+            for (int i = 0; i < ch.Length; i++)
+            {
+                if (max < ch[i])
+                {
+                    max = ch[i];
+                }
+            }
+            txtGTLN.Text = max.ToString();
+        }
+    }
+}
